2b8a808bf01fb4806a41a3d94d33c8798c8149cc
agtkh <108037747+agtkh@users.noreply.github.com>
2023-05-08 15:58:17 +0900

    Work 5/8

--- dns.c
+++ dns.c
@@ -14,22 +14,76 @@
 
 // DNSパケットヘッダ
 struct dns_header {
-    u_int16_t id;  // 識別番号
-    u_int16_t flags; // フラグ
-    u_int16_t qdcount;  // 問い合わせエントリの数
-    u_int16_t ancount;  // 応答エントリの数
+    u_int16_t id;       // 識別番号
+    u_int16_t flags;    // フラグ
+    u_int16_t qdcount;  // Queryエントリの数
+    u_int16_t ancount;  // Answerエントリの数
     u_int16_t nscount;  // オーソリティの数
     u_int16_t arcount;  // 追加情報の数
 };
 
-// 問い合わせエントリ
+// DNSレコードタイプ
+enum dns_record_type {
+    DNS_TYPE_A = 1,      // ホストアドレス
+    DNS_TYPE_NS = 2,     // ネームサーバ
+    DNS_TYPE_CNAME = 5,  // エイリアス
+    DNS_TYPE_SOA = 6,    // スタートオブオーソリティ
+    DNS_TYPE_PTR = 12,   // ポインタ
+    DNS_TYPE_MX = 15,    // メールエクスチェンジャ
+    DNS_TYPE_TXT = 16,   // テキスト
+    DNS_TYPE_AAAA = 28,  // IPv6ホストアドレス
+    DNS_TYPE_SRV = 33,   // サービス
+    DNS_TYPE_ANY = 255   // 任意の型
+};
+
+// DNSレコードタイプを文字列に変換する関数
+char *dns_type_to_str(u_int16_t type, char *output, int output_size) {
+    switch (type) {
+        case DNS_TYPE_A:
+            strncpy(output, "A", output_size);
+            break;
+        case DNS_TYPE_NS:
+            strncpy(output, "NS", output_size);
+            break;
+        case DNS_TYPE_CNAME:
+            strncpy(output, "CNAME", output_size);
+            break;
+        case DNS_TYPE_SOA:
+            strncpy(output, "SOA", output_size);
+            break;
+        case DNS_TYPE_PTR:
+            strncpy(output, "PTR", output_size);
+            break;
+        case DNS_TYPE_MX:
+            strncpy(output, "MX", output_size);
+            break;
+        case DNS_TYPE_TXT:
+            strncpy(output, "TXT", output_size);
+            break;
+        case DNS_TYPE_AAAA:
+            strncpy(output, "AAAA", output_size);
+            break;
+        case DNS_TYPE_SRV:
+            strncpy(output, "SRV", output_size);
+            break;
+        case DNS_TYPE_ANY:
+            strncpy(output, "ANY", output_size);
+            break;
+        default:
+            strncpy(output, "UNKNOWN", output_size);
+            break;
+    }
+    return output;
+}
+
+// Queryエントリ
 struct dns_query {
     char *qname;  // 照会名 (可変長,0で終端)
     u_int16_t qtype;
     u_int16_t qclass;
 };
 
-// 応答エントリ
+// Answerエントリ
 struct dns_answer {
     char *name;
     u_int16_t type;
@@ -40,7 +94,7 @@ struct dns_answer {
 };
 
 /**
- * @brief データを送信し、その応答を受信する関数。戻り値は受信した応答のサイズ
+ * @brief データをUDPで送信し、その応答を受信する関数。戻り値は受信した応答のサイズ
  *
  * @param[in] addr 送信先アドレス
  * @param[in] port 送信先ポート番号
@@ -86,50 +140,80 @@ int udp_send(char *addr, int port, void *data, int data_size, void *recv_buf, in
 }
 
 /**
- * @brief 問い合わせエントリを生成する関数
+ * @brief Queryエントリを生成する関数
  *
  * @param[in] qname 照会名 (必ず'\0'で終端)
- * @param[in] qtype 問い合わせの種類
- * @param[in] qclass 問い合わせのクラス
+ * @param[in] qtype Queryの種類
+ * @param[in] qclass Queryのクラス
  * @param[out] buf 出力用バッファ
  * @param[in] buf_size 出力用バッファのサイズ
  * @return int 生成したデータのサイズ。失敗時は-1
  */
-int _dns_gen_query(char *qname, u_int16_t qtype, u_int16_t qclass, char *buf, int buf_size) {
-    // 可変長エントリqnameの追加していく
-    u_int16_t *label_len = (u_int16_t *)buf;
-    *label_len = 0;
-    int data_size = 1;
-    while (qname[data_size - 1] != '\0') {
-        if (data_size >= buf_size) {
-            fprintf(stderr, "buf size over\n");
-            return -1;
-        }
-        if (qname[data_size - 1] == '.') {
-            label_len = (u_int16_t *)(buf + data_size);
-            *label_len = 0;
-        } else {
-            buf[data_size] = qname[data_size - 1];
-            *label_len += 1;
-        }
-        data_size++;
-    }
-    buf[data_size++] = 0;  // 終端
+int dns_gen_query(char *qname, u_int16_t qtype, char *buf, int buf_size) {
+    // 可変長qnameエントリの追加
+    int data_size = dns_encode_name(qname, buf, buf_size - 4);
+    if (data_size == -1) return -1;
 
     // 固定長エントリqtypeとqclassの追加
     *((u_int16_t *)(buf + data_size)) = htons(qtype);
-    data_size += sizeof(qtype);
-    *((u_int16_t *)(buf + data_size)) = htons(qclass);
-    data_size += sizeof(qclass);
+    *((u_int16_t *)(buf + data_size + 2)) = htons(1);  // IN
+    data_size += 4;
 
     return data_size;
 }
 
+// 名前をqnameエントリ用の形式に変換する関数
+int dns_encode_name(char *name, char *output, int output_size) {
+    u_int16_t *label_len_ptr = (u_int16_t *)output;
+    *label_len_ptr = 0;
+    int output_index = 1;
+    int name_len = strlen(name);
+    for (int i = 0; i < name_len; i++) {
+        if (output_index >= output_size - 1) {
+            fprintf(stderr, "output size over\n");
+            return -1;
+        }
+        if (name[i] == '.') {
+            label_len_ptr = (u_int16_t *)(output + output_index);
+            *label_len_ptr = 0;
+        } else {
+            output[output_index] = name[i];
+            *label_len_ptr += 1;
+        }
+        output_index++;
+    }
+    output[output_index++] = 0;  // 終端
+    return output_index;
+}
+
+int dns_decode_name(char *data, char *output, int output_size) {
+    int output_index = 0;
+    int data_index = 0;
+    while (data[data_index] != 0) {
+        if (data[data_index] >= 0xc0) {
+            // ポインタ
+            int ptr = ntohs(*((u_int16_t *)(data + data_index))) & 0x3fff;
+            dns_decode_name(data + ptr, output + output_index, output_size - output_index);
+            return output_index;
+        } else {
+            // ラベル
+            int label_len = data[data_index];
+            for (int i = 0; i < label_len; i++) {
+                output[output_index++] = data[data_index + i + 1];
+            }
+            output[output_index++] = '.';
+            data_index += label_len + 1;
+        }
+    }
+    output[output_index++] = 0;
+    return output_index;
+}
+
 /**
- * @brief 問い合わせエントリを構造体にパースして返す関数
+ * @brief Queryエントリを構造体にパースして返す関数
  *
- * @param data パース対象のデータ
- * @param query パース結果の構造体を格納する変数
+ * @param[in] data パース対象のデータ
+ * @param[out] query パース結果の構造体を格納する変数
  * @return int パースしたデータのサイズ。
  */
 int dns_parse_query(char *data, struct dns_query *query) {
@@ -148,11 +232,11 @@ int dns_parse_query(char *data, struct dns_query *query) {
 }
 
 /**
- * @brief 応答エントリを構造体にパースする関数
+ * @brief Answerエントリを構造体にパースする関数
  *
- * @param recv_data 受信データ全体
- * @param data パース対象の応答エントリデータ
- * @param answer パース結果の構造体を格納する変数
+ * @param[in] recv_data 受信データ全体
+ * @param[in] data パース対象のAnswerエントリデータ
+ * @param[out] answer パース結果の構造体を格納する変数
  * @return int パースしたデータのサイズ。
  */
 int dns_parse_answer(char *recv_data, char *data, struct dns_answer *answer) {
@@ -189,22 +273,35 @@ void print_hex(char *data, int size) {
     }
     printf("\n");
 }
+
 /**
  * @brief アドレスを文字列として表示する関数(デバッグ用)
- * 
+ *
  * @param data アドレス
  * @param size アドレスのサイズ
  */
 void print_addr(char *data, int size) {
-    for (int i = 0; i < size; i++) {
-        if (i != 0) { printf("."); }
-        printf("%d", (u_int8_t)data[i]);
+    if (size == 4) {
+        for (int i = 0; i < 4; i++) {
+            if (i != 0) {
+                printf(".");
+            }
+            printf("%d", (u_int8_t)data[i]);
+        }
+    } else if (size == 16) {
+        for (int i = 0; i < 16; i += 2) {
+            if (i != 0) {
+                printf(":");
+            }
+            printf("%02x%02x", (u_int8_t)data[i], (u_int8_t)data[i + 1]);
+        }
+    } else {
+        fprintf(stderr, "invalid addr size");
     }
     printf("\n");
 }
 
 int dns_request(char *name, char *dns_addr, int dns_port) {
-
     char send_buf[512];
     int send_buf_size = 0;
 
@@ -214,19 +311,17 @@ int dns_request(char *name, char *dns_addr, int dns_port) {
     memset(header, 0, sizeof(*header));
     header->id = htons(0x4649);
     header->flags = 0x0100;
-    header->qdcount = htons(1);  // 問い合わせエントリ数
+    header->qdcount = htons(1);  // Queryエントリ数
 
-    // 問い合わせデータを生成し追加
-    int data_size = _dns_gen_query(name, 1, 1, send_buf + send_buf_size, sizeof(send_buf) - send_buf_size);
+    // Queryエントリを生成し追加
+    int data_size = dns_gen_query(name, DNS_TYPE_AAAA, send_buf + send_buf_size, sizeof(send_buf) - send_buf_size);
     if (data_size == -1) {
         fprintf(stderr, "qname error\n");
         return -1;
     }
     send_buf_size += data_size;
 
-    // printf("send: ");
-    // print_hex(send_buf, send_buf_size);
-
+    // データを送信&受信
     char recv_buf[BUF_SIZE];
     int recv_size = udp_send(dns_addr, dns_port, send_buf, send_buf_size, recv_buf, sizeof(recv_buf));
     if (recv_size == -1) {
@@ -236,34 +331,32 @@ int dns_request(char *name, char *dns_addr, int dns_port) {
 
     int parsed_size = 0;
 
-    // ヘッダのパース
+    // 受信したデータのヘッダのパース
     struct dns_header *recv_header = (struct dns_header *)recv_buf;
-    int qdcount = ntohs(recv_header->qdcount);
-    int ancount = ntohs(recv_header->ancount);
+    int qdcount = ntohs(recv_header->qdcount);  // Queryエントリ数
+    int ancount = ntohs(recv_header->ancount);  // Answerエントリ数
     parsed_size += sizeof(struct dns_header);
 
-    // 問い合わせエントリのパース
+    // Queryエントリのパース
     for (int i = 0; i < qdcount; i++) {
-        // printf("query #%d:\n", i+1);
         struct dns_query query;
         parsed_size += dns_parse_query(recv_buf + parsed_size, &query);
-        // printf("qname: %s\n", query.qname);
-        // printf("qtype: %d\n", ntohs(query.qtype));
-        // printf("qclass: %d\n", ntohs(query.qclass));
-        // printf("\n");
     }
 
-    // 応答エントリのパース
+    // Answerエントリのパース
     for (int i = 0; i < ancount; i++) {
-        printf("answer #%d:\n", i+1);
+        printf("\n-- answer #%d --\n", i + 1);
+
         struct dns_answer answer;
         parsed_size += dns_parse_answer(recv_buf, recv_buf + parsed_size, &answer);
+
         printf("name: %s\n", answer.name);
-        printf("type: %d\n", ntohs(answer.type));
-        printf("class: %d\n", ntohs(answer.class));
-        printf("ttl: %d\n", ntohl(answer.ttl));
-        printf("rdlength: %d\n", ntohs(answer.rdlength));
-        printf("rdata: https://");
+
+        char type_str[10];
+        dns_type_to_str(ntohs(answer.type), type_str, sizeof(type_str));
+        printf("type: %s\n", type_str);
+
+        printf("result: ");
         print_addr(answer.rdata, ntohs(answer.rdlength));
     }
 
